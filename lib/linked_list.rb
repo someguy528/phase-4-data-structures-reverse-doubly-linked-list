6bfe601411d5e5f138eeda418e24f2b9d433e7f2
Brandon Castro <castroyou999@gmail.com>
2023-02-26 01:27:25 -0500

    commit tree 3a7a4b85c5f1e7227c379b803e975f4de5caa7ed

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -10,5 +10,44 @@ class LinkedList
 
   def reverse!
     # your code here
+    # node = self.head
+    # node.next_node = nil
+    # return if @head == nil && @tail == nil || @head == @tail
+    # node = self.tail.prev_node
+    # prev = self.tail
+    # self.head = prev
+    # self.head.prev_node = nil
+    # prev.next_node = node
+    # while node
+    #   # reverse order
+    #   node.next_node = node.prev_node
+    #   # set new order
+    #   node.prev_node = prev
+    #   # advance to next node
+    #   prev = node 
+    #   node = node.next_node
+    # end
+    # # while ends when node == nil, prev is final entry
+    # self.tail = prev
+    return if @head == nil && @tail == nil || @head == @tail
+
+    node = self.tail
+    prev = nil
+    self.head = node
+    # self.head.prev_node = nil
+    # prev.next_node = node
+    while node
+      # reverse order
+      node.next_node = node.prev_node
+      # set new order
+      node.prev_node = prev
+      # advance to next node
+      prev = node 
+      node = node.next_node
+    end
+    # while ends when node == nil, prev is final entry
+    self.tail = prev
+
   end
+
 end
